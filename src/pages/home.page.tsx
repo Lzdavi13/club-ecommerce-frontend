2b876ca7d43ac61ae509015946a3e8234f254222
unknown <lzdavi1325@gmail.com>
2025-06-14 11:40:45 -0300

    feat: add Category Type and data

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,7 +1,14 @@
-import Header from "../components/header.component";
+import Categories from "../components/categories/categorie.component";
+import Header from "../components/header/header.component";
 
 function HomePage() {
-  return <Header/>;
+  return (
+    <>
+      <Header />
+
+      <Categories />
+    </>
+  );
 }
 
 export default HomePage;
