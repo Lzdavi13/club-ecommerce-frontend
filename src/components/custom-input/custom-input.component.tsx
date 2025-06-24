5832aa8a25635897d939b18abe0bef268ebc31d5
unknown <lzdavi1325@gmail.com>
2025-06-24 14:35:34 -0300

    feat: add Custom Input to Login Page

--- src/components/custom-input/custom-input.component.tsx
+++ src/components/custom-input/custom-input.component.tsx
@@ -1,12 +1,12 @@
 import type { InputHTMLAttributes } from "react";
 import { CustomInputContainer } from "./custom-input.styles";
 
-interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement>{
-  hasError?: boolean
+interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean;
 }
 
-function CustomInput({hasError, ...rest}: CustomInputProps) {
-  return <CustomInputContainer hasError={hasError} {...rest}/>
+function CustomInput({ hasError, ...rest }: CustomInputProps) {
+  return <CustomInputContainer hasError={hasError} {...rest} />;
 }
 
 export default CustomInput;
--- src/components/custom-input/custom-input.styles.ts
+++ src/components/custom-input/custom-input.styles.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
-import Colors from '../../theme/theme.colors'
+import styled from "styled-components";
+import Colors from "../../theme/theme.colors";
 
 type CustomInputContainerProps = {
-  hasError?: boolean
-}
+  hasError?: boolean;
+};
 
 export const CustomInputContainer = styled.input<CustomInputContainerProps>`
   border: none;
@@ -16,7 +16,7 @@ export const CustomInputContainer = styled.input<CustomInputContainerProps>`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   color: ${Colors.text.dark};
-  border: ${(props) => (props.hasError ? `2px solid ${Colors.error}` : 'none')};
+  border: ${(props) => (props.hasError ? `2px solid ${Colors.error}` : "none")};
 
   &::placeholder {
     color: ${(props) =>
@@ -25,6 +25,6 @@ export const CustomInputContainer = styled.input<CustomInputContainerProps>`
 
   &:focus {
     outline: ${(props) =>
-      props.hasError ? 'none' : `2px solid ${Colors.input.placeholder}`};
+      props.hasError ? "none" : `2px solid ${Colors.input.placeholder}`};
   }
-`
+`;
